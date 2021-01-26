9673117dd270970aad78d6ec2d2bfb09f5d9afdf
Muhidinov Manuchehr <manuchehr.muhidinov@gmail.com>
2021-01-26 21:20:20 +0500

    all fixtures add

--- src/Command/command_fixtures.php
+++ src/Command/command_fixtures.php
@@ -2,6 +2,8 @@
 
 include_once __DIR__ . "/../Fixtures/Fixture01.php";
 include_once __DIR__ . "/../Fixtures/Fixture02.php";
+include_once __DIR__ . "/../Fixtures/Fixture03.php";
+include_once __DIR__ . "/../Fixtures/Fixture04.php";
 include_once __DIR__ . "/../Service/DBConnector.php";
 
 //$fixture = new Fixture01(DBConnector::getInstance());
@@ -9,5 +11,11 @@ include_once __DIR__ . "/../Service/DBConnector.php";
 
 //$fixture2 = new Fixture02(DBConnector::getInstance());
 //$fixture2->run();
+
+//$fixture3 = new Fixture03(DBConnector::getInstance());
+//$fixture3->run();
+
+$fixture4 = new Fixture04(DBConnector::getInstance());
+$fixture4->run();
 die("Ok");
 
