88573efaa5e2c17b290e239067f864920dfdb839
Muhidinov Manuchehr <manuchehr.muhidinov@gmail.com>
2021-01-27 10:15:15 +0500

    Add Migration prior to category

--- src/Command/command_fixtures.php
+++ src/Command/command_fixtures.php
@@ -6,16 +6,17 @@ include_once __DIR__ . "/../Fixtures/Fixture03.php";
 include_once __DIR__ . "/../Fixtures/Fixture04.php";
 include_once __DIR__ . "/../Service/DBConnector.php";
 
-//$fixture = new Fixture01(DBConnector::getInstance());
-//$fixture->run();
+$fixture = new Fixture01(DBConnector::getInstance());
+$fixture->run();
 
-//$fixture2 = new Fixture02(DBConnector::getInstance());
-//$fixture2->run();
+$fixture2 = new Fixture02(DBConnector::getInstance());
+$fixture2->run();
 
-//$fixture3 = new Fixture03(DBConnector::getInstance());
-//$fixture3->run();
+$fixture3 = new Fixture03(DBConnector::getInstance());
+$fixture3->run();
 
 $fixture4 = new Fixture04(DBConnector::getInstance());
 $fixture4->run();
+
 die("Ok");
 
