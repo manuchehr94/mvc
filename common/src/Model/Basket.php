8709f110db714da5fa51a8aaf0f2e4651c63bd7a
Muhidinov Manuchehr <manuchehr.muhidinov@gmail.com>
2021-02-06 13:16:53 +0500

    The end of stream 18

---
+++ common/src/Model/Basket.php
@@ -0,0 +1,41 @@
+<?php
+
+include_once __DIR__ . "/../Service/DBConnector.php";
+
+class Basket
+{
+    public $id;
+    public $userId;
+    public $items  = [];
+
+    private $conn;
+
+    public function __construct($userId = null)
+    {
+        $this->conn = DBConnector::getInstance()->connect();
+        $this->userId = $userId;
+    }
+
+    public function save()
+    {
+        $query = "INSERT INTO basket (id, user_id) VALUES ( null, '" . $this->userId . "')";
+        $result = mysqli_query($this->conn, $query);
+
+        if(!$result) {
+            throw new Exception(mysqli_error($this->conn));
+        }
+    }
+
+    public function getByUserId($userId)
+    {
+        $result = mysqli_query($this->conn, "Select * from basket where user_id = $userId limit 1");
+        $oneProduct = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        return reset($oneProduct);
+    }
+
+    public function deleteByUserId($userId)
+    {
+        return mysqli_query($this->conn, "delete from basket where user_id = $userId limit 1");
+    }
+
+}
---
+++ common/src/Model/BasketItem.php
@@ -0,0 +1,57 @@
+<?php
+
+include_once __DIR__ . "/../Service/DBConnector.php";
+
+class BasketItem
+{
+    public $id;
+    public $basketId;
+    public $productId;
+    public $quantity;
+
+    private $conn;
+
+    public function __construct(
+        $basketId = null,
+        $productId = null,
+        $quantity = null
+    )
+    {
+        $this->conn = DBConnector::getInstance()->connect();
+        $this->basketId = $basketId;
+        $this->productId = $productId;
+        $this->quantity = $quantity;
+    }
+
+    public function save()
+    {
+        if($this->id) {
+            $query = "UPDATE basket_item set quantity=" .
+                $this->quantity ." WHERE id = " . $this->id . " LIMIT 1";
+        } else {
+            $query = "INSERT INTO basket_item (id, basket_id, product_id, quantity) VALUES ( 
+                null, '" . $this->basketId . "', '" . $this->productId . "', '" . $this->quantity . "'
+                        
+            )";
+        }
+
+        $result = mysqli_query($this->conn, $query);
+
+        if(!$result) {
+            throw new Exception(mysqli_error($this->conn));
+        }
+    }
+
+    public function getByBasketId($basketId)
+    {
+        $result = mysqli_query($this->conn, "Select * from basket_item where basket_id = $basketId limit 1");
+        $oneProduct = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        return reset($oneProduct);
+    }
+
+    public function deleteByBasketId($basketId)
+    {
+        return mysqli_query($this->conn, "delete from basket_item where basket_id = $basketId limit 1");
+    }
+
+}
