cb64248f453c6ce65d1c77d66e840440fd1acc0a
Muhidinov Manuchehr <manuchehr.muhidinov@gmail.com>
2021-02-06 16:39:27 +0500

    Added error.log

--- common/src/Service/ExceptionService.php
+++ common/src/Service/ExceptionService.php
@@ -6,7 +6,8 @@ class ExceptionService
     public static function error(Exception $e, $side)
     {
         http_response_code($e->getCode());
-        //error_log
+
+        error_log($e->getMessage() . ", file = ProductController.php");
         $code = $e->getCode();
         $message = $e->getMessage();
 
